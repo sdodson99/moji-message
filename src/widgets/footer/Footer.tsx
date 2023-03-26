df9253eec254f74edd5ba9c14a98ea74ecd527a3
Sean Dodson <dodsonsean@comcast.net>
2023-03-26 09:50:44 -0400

    Update year in footer

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="bg-gray-50 py-4 text-center text-xs">
       <div className="container">
-        Copyright © 2022 Sean Dodson. All rights reserved.
+        Copyright © {new Date().getFullYear()} Sean Dodson. All rights reserved.
       </div>
     </footer>
   );
