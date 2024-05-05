e166f6bfb8bb27f5bf97411af5ea9aa0eb9ec553
sdodson <sc.dodson4@gmail.com>
2024-05-05 14:17:20 -0400

    Add subtle curve to landing

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,7 +2,6 @@ import { Header } from '@/widgets/header';
 import { Footer } from '@/widgets/footer';
 import { ConvertToMojiMessage } from '@/widgets/convert-to-moji-message';
 import styles from './index.module.css';
-import classNames from 'classnames';
 import { useRef } from 'react';
 
 export default function Index() {
@@ -13,31 +12,40 @@ export default function Index() {
       <div className="min-h-screen">
         <Header className={styles.headerHeight} />
         <main>
-          <section
-            className={classNames(
-              styles.remainingHeightAboveFold,
-              'bg-primary py-8 text-white'
-            )}
-          >
-            <div className="container">
-              <h1 className="text-5xl font-bold md:text-7xl">
-                The <span className="text-yellow-300">Fun</span> Way to Create a
-                Message
-              </h1>
-              <p className="mt-8 text-lg md:text-2xl">
-                Encode a message with emojis to spice up your delivery and make
-                your message stick.
-              </p>
-              <div className="mt-8">
-                <button
-                  className="rounded-lg bg-white px-8 py-4 text-slate-800 transition hover:bg-slate-200"
-                  onClick={() =>
-                    formRef?.current?.scrollIntoView({ behavior: 'smooth' })
-                  }
-                >
-                  Get Started
-                </button>
+          <section className={styles.remainingHeightAboveFold}>
+            <div className="bg-primary pt-8 pb-16 text-white">
+              <div className="container">
+                <h1 className="text-5xl font-bold md:text-7xl">
+                  The <span className="text-yellow-300">Fun</span> Way to Create
+                  a Message
+                </h1>
+                <p className="mt-8 text-lg md:text-2xl">
+                  Encode a message with emojis to spice up your delivery and
+                  make your message stick.
+                </p>
+                <div className="mt-8">
+                  <button
+                    className="rounded-lg bg-white px-8 py-4 text-slate-800 transition hover:bg-slate-200"
+                    onClick={() =>
+                      formRef?.current?.scrollIntoView({ behavior: 'smooth' })
+                    }
+                  >
+                    Get Started
+                  </button>
+                </div>
               </div>
+            </div>
+            <div className={styles.wave}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1200 120"
+                preserveAspectRatio="none"
+              >
+                <path
+                  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                  className="fill-primary"
+                ></path>
+              </svg>
             </div>
           </section>
           <section ref={formRef} className="container min-h-screen py-24">
