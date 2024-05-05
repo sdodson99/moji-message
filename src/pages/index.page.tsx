6a34eb46f3bd9a13d4974814759e82e2802eaa7b
sdodson <sc.dodson4@gmail.com>
2024-05-04 20:41:29 -0400

    Smooth scroll to form

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -3,8 +3,11 @@ import { Footer } from '@/widgets/footer';
 import { ConvertToMojiMessage } from '@/widgets/convert-to-moji-message';
 import styles from './index.module.css';
 import classNames from 'classnames';
+import { useRef } from 'react';
 
 export default function Index() {
+  const formRef = useRef<HTMLFormElement>(null);
+
   return (
     <div className={styles.layout}>
       <div className="min-h-screen">
@@ -26,13 +29,18 @@ export default function Index() {
                 your message stick.
               </p>
               <div className="mt-8">
-                <button className="rounded-lg bg-white px-8 py-4 text-slate-800 transition hover:bg-slate-200">
+                <button
+                  className="rounded-lg bg-white px-8 py-4 text-slate-800 transition hover:bg-slate-200"
+                  onClick={() =>
+                    formRef?.current?.scrollIntoView({ behavior: 'smooth' })
+                  }
+                >
                   Get Started
                 </button>
               </div>
             </div>
           </section>
-          <section className="container my-8">
+          <section ref={formRef} className="container min-h-screen py-24">
             <h2 className="sr-only">Create a Moji Message</h2>
             <ConvertToMojiMessage />
           </section>
